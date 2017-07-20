e65b32098b67275fe9a3716c714d46a9d6719389
sharon smith <sharon.e.smith2@gmail.com>
2017-07-20 17:58:24 -0500

    coded for homework and in class demo

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Classes
+{
+    public class Employee
+    {
+        //methods to get/set employee name, job title, and start date
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime StartDate  { get; set; }
+    }
+}
